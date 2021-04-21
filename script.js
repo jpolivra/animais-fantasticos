fb20e4f702771bb944d8ee3954d733688237287f
thxjao <jpso.dev@gmail.com>
2021-04-21 11:38:55 -0300

    Exercise selection

---
+++ script.js
@@ -0,0 +1,15 @@
+
+const imagens = document.querySelectorAll('img');
+console.log(imagens);
+
+//
+
+const imagemAnimal = document.querySelectorAll('img[src^="./img/imagem"]');
+console.log(imagemAnimal);
+
+
+//
+
+const primeiroH2 = document.querySelector('.animais-descricao h2');
+console.log(primeiroH2);
+
