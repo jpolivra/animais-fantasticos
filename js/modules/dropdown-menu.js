ba24d001006171288316c2f4fbe99704c8820330
João Pedro Silva <thxjao.dev@gmail.com>
2021-05-24 08:40:56 -0300

    Added dropdown menu script

---
+++ js/modules/dropdown-menu.js
@@ -0,0 +1,14 @@
+export default function initDropdownMenu() {}
+
+const dropdownMenus = document.querySelectorAll("[data-dropdown]");
+
+dropdownMenus.forEach((item) => {
+  ["touchstart", "click"].forEach((userEvent) => {
+    item.addEventListener(userEvent, handleClick);
+  });
+});
+
+function handleClick(event) {
+  event.preventDefault();
+  this.classList.add("active");
+}
